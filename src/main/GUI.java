25886da161e8558e76989587c1431f23bfd20aee
Jacob Clark <clarkjac03@gmail.com>
2024-09-05 11:58:33 -0400

    Add Buttons, Place 0-3 buttons

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -6,14 +6,44 @@ import java.awt.event.ActionListener;
 
 public class GUI implements ActionListener {
     JFrame frame = new JFrame();
-    JButton testButton = new JButton("test");
+    JButton oneButton = new JButton("1");
+    JButton zeroButton = new JButton("0");
+    JButton twoButton = new JButton("2");
+    JButton threeButton = new JButton("3");
+    JButton equalsButton = new JButton("=");
+    JButton plusButton = new JButton("+");
+    JButton minusButton = new JButton("-");
+    JButton multButton = new JButton("*");
+    JButton divButton = new JButton("/");
+    JButton squareButton = new JButton("^");
+    JButton sqrtButton = new JButton("√");
+
+    int defaultButtonWidth = 80;
+    int defaultButtonHeight = 30;
+    int buttonSpacing = 20;
+
     GUI(){
 
-        testButton.setBounds(100,160,200,40);
-        testButton.setFocusable(false);
-        testButton.addActionListener(this);
+        zeroButton.setBounds(10,420,defaultButtonWidth,defaultButtonHeight);
+        zeroButton.setFocusable(false);
+        zeroButton.addActionListener(this);
 
-        frame.add(testButton);
+        oneButton.setBounds(defaultButtonWidth + buttonSpacing,420,defaultButtonWidth,defaultButtonHeight);
+        oneButton.setFocusable(false);
+        oneButton.addActionListener(this);
+
+        twoButton.setBounds(10,420-buttonSpacing*2,defaultButtonWidth,defaultButtonHeight);
+        twoButton.setFocusable(false);
+        twoButton.addActionListener(this);
+
+        threeButton.setBounds(defaultButtonWidth+buttonSpacing,420-buttonSpacing*2,defaultButtonWidth,defaultButtonHeight);
+        threeButton.setFocusable(false);
+        threeButton.addActionListener(this);
+
+        frame.add(zeroButton);
+        frame.add(oneButton);
+        frame.add(twoButton);
+        frame.add(threeButton);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
